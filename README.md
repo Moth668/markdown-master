704ec7046731da177861e40b22af9b2cfe59b966
Moth <tdrice668@gmail.com>
2024-09-22 22:23:12 -0500

    made updates to index.js to include full license list, updated README.md

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
                 # Markdown Master
                 
                 ## Description
-                j
+                This project is designed to create a professional README.md file to be added to your GitHub Repositories.
 
                 # Table of Contents
                 undefined
@@ -11,19 +11,19 @@
                 undefined
                 
                 ## Usage
-                j
+                  run node . to run the generator. It will ask you specific questions to build your README.md file. 
 
                 ## License
                 MIT
                 
                 ## Contributing
-                j
+                Please contribute.
                 
                 ## Tests
-                j
+                You should receive an error message if there is a problem writing to the file. Otherwise your message will say "File written successfully
 
                 ## Questions
-                j
-                j
+                 Moth668
+                tdrice668@gmail.com
                 ### use this contact information to reach me with additional questions. 
                         
--- index.js
+++ index.js
@@ -41,6 +41,11 @@ const questions = [
     },
     {
         type: 'input',
+        name: 'link',
+        message: 'Paste the link to your GitHub profile.'
+    },
+    {
+        type: 'input',
         name: 'email',
         message: 'Enter your email address.',
     },
@@ -48,7 +53,9 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'Choose a license for your application from the list of options.',
-        choices: ['MIT', 'GPL-3.0' , 'LGPL'],
+        choices: ['AFL-3.0', 'Apache-2.0', 'Artistic-2.0', 'BSL-1.0', 'BSD-2-Clause', 'BSD-3-Clause-Clear', 'BSD-4-Clause', '0BSD', 'CC', 'CC0-1.0', 'CC-BY-4.0', 'CC-BY-SA-4.0', 'WTFPL', 'ECL-2.0', 
+            'EPL-2.0', 'EUPL-1.1', 'AGPL-3.0', 'GPL', 'GPL-2.0', 'GPL-3.0', 'LGPL', 'LGPL-2.1', 'LGPL-3.0', 'ISC', 'LPPL-1.3c', 'MS-PL', 'MIT', 'MPL-2.0', 'OSL-3.0', 'PostgreSQL', 'OFL-1.1', 'NCSA',
+            'Unlicense', 'Zlib'],
     }
 ];
 
@@ -63,11 +70,10 @@ function writeToFile(fileName, data) {
     });
 }
 
-// TODO: Create a function to initialize app
+// TODO: Create a function to initialize app 
 function init() {
     inquirer.prompt(questions).then((answers) => {
-        console.log(answers);
-        // Convert answers to a string format suitable for README
+        // console.log(answers);
         const readmeContent = `
                 # ${answers.projectName}
                 
